d6dc5244349c71c1b70f3b4273ae8a9408dea13b
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2019-10-02 10:47:11 +0300

    PS-10910 Fixed architecture issues

--- src/Spryker/Zed/PriceProductMerchantRelationshipGui/Dependency/Facade/PriceProductMerchantRelationshipGuiToMerchantRelationshipFacadeBridge.php
+++ src/Spryker/Zed/PriceProductMerchantRelationshipGui/Dependency/Facade/PriceProductMerchantRelationshipGuiToMerchantRelationshipFacadeBridge.php
@@ -25,11 +25,11 @@ class PriceProductMerchantRelationshipGuiToMerchantRelationshipFacadeBridge impl
     }
 
     /**
-     * @param \Generated\Shared\Transfer\MerchantRelationshipFilterTransfer $merchantRelationshipFilterTransfer
+     * @param \Generated\Shared\Transfer\MerchantRelationshipFilterTransfer|null $merchantRelationshipFilterTransfer
      *
      * @return \Generated\Shared\Transfer\MerchantRelationshipTransfer[]
      */
-    public function getMerchantRelationshipCollection(MerchantRelationshipFilterTransfer $merchantRelationshipFilterTransfer): array
+    public function getMerchantRelationshipCollection(?MerchantRelationshipFilterTransfer $merchantRelationshipFilterTransfer): array
     {
         return $this->merchantRelationshipFacade->getMerchantRelationshipCollection($merchantRelationshipFilterTransfer);
     }
